3fad9ebb4d2bd8d39ed0381888da25b5b95e429e
NightTrain <prkmms@gmail.com>
2013-10-29 20:53:40 -0400

    Edited layout

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131029010533) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "goals", force: true do |t|
     t.integer  "hotspot_id"
     t.string   "time"
